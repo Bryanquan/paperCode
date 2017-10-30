b0b0dbea20ddedf2ae3430b0de3d9e3ba6d12b1d
bryan quan <513296742@qq.com>
2017-10-30 09:57:23 +0800

    Update lorenz.m

--- lorenz.m
+++ lorenz.m
@@ -1,3 +1,5 @@
+% we can set different values for the diagonal elements of the diagonal matrix in our comparative results 
+
 function dm=lorenz(t,m)
 a=10;
 b=28;
@@ -8,24 +10,24 @@ k2=-100;
 k3=-100;
 dm=zeros(6,1);
 
-%驱动系统
+%the driving system
 dm(1)=a*(m(2)-m(1));
 dm(2)=b*m(1)-m(2)-m(1)*m(3);
 dm(3)=m(1)*m(2)-c*m(3);
 
-%控制器的设计
+% desigin of controller
 u1=h*a*(m(2)-m(1))+k1*(m(4)-h*m(1))-a*(m(4)-h*m(1));
 u2=b*h*m(1)-h*m(1)*m(3)-h*m(2)+m(4)*m(6)+m(5)+k2*(m(5)-h*m(2))-b*m(4);
 u3=-h*c*m(3)+h*m(1)*m(2)-m(4)*m(5)+k3*(m(6)-h*m(3))+c*m(6);
 
 
-%响应系统
+%the response system
 dm(4)=m(7)*(m(5)-m(4))+u1;
 dm(5)=m(8)*m(4)-m(5)-m(4)*m(6)+u2;
 dm(6)=m(4)*m(5)-m(9)*m(6)+u3;
 
 
-%参数自适应
+%Parameter adaptive
 dm(7)=(m(4)-m(5))*(m(4)-h*m(1));
 dm(8)=-m(4)*(m(5)-h*m(2));
 dm(9)=m(6)*(m(6)-h*m(3));
